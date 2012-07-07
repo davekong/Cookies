2b0c4ce7ea0188fcfb7ed186e70dd87afbb2e809
Amy <oxling@gmail.com>
2012-07-07 16:45:47 -0400

    Added Viterbi model; added sample code (viterbi.py)

--- cookies.py
+++ cookies.py
@@ -1,17 +1,16 @@
+from __future__ import division
 import os
 
 #Tag types
-Empty = 0
-Unit = 1
-Quantity = 2
-Food = 3
-Modifier = 4
-Time = 5
-Degrees = 6
-Boring = 7
+Unit = 0
+Quantity = 1
+Food = 2
+Modifier = 3
+Time = 4
+Degrees = 5
+Boring = 6
 
 tokens = {
-	"e" : Empty,
 	"u" : Unit,
 	"q" : Quantity,
 	"f" : Food,
@@ -21,32 +20,44 @@ tokens = {
 	"b" : Boring
 }
 
-token_description = ["E", "U", "Q", "F", "M", "T", "D", "B"]
-token_description_verbose = ["Empty", "Unit", "Quantity", "Food", "Modifier", "Time", "Degrees", "Boring"]
+token_description = ["U", "Q", "F", "M", "T", "D", "B"]
+token_description_verbose = ["Unit", "Quantity", "Food", "Modifier", "Time", "Degrees", "Boring"]
 
-token_incidence = {}
 tuple_probability = []
+number_probability = []
+start_tag_count = []
+tag_count = []
 parsed_lines = []
 
 # Go through a tagged file and split it into
 # (word, token) tuples, line by line.
 # Appends results to parsed_lines array
 def parse_learning_data(f):
+	parsed_line = []
 	for line in f:
-		parsed_line = []
 		for word in line.split():
 			word_parts = word.split("_")
+			token = Boring
 			if (len(word_parts) > 1):
 				token = tokens.get(word_parts[1][0], Boring)
-				parsed_line.append((word_parts[0], token))
-			else:
-				parsed_line.append((word_parts[0], Boring))
+			parsed_line.append((word_parts[0], token))
 		parsed_lines.append(parsed_line)
 
+def is_number(word):
+	if len(word) > 0:
+		try:
+			float(word[0])
+			return True
+		except ValueError:
+			return False
+
 # Initialize a table to hold probability relationships
-for row in range(8):
+for row in range(7):
 	tuple_probability.append([])
-	for col in range(8):
+	number_probability.append(0)
+	tag_count.append(0)
+	start_tag_count.append(0)
+	for col in range(7):
 		tuple_probability[row].append(0)
 
 # Load all the tagged_recipes file
@@ -57,55 +68,96 @@ for f in files:
 # Calculate the probability of a tuple occurance
 for line in parsed_lines:
 	for i in range(len(line)):
-		gram1 = ("", Empty)
-		if i > 0:
+		if i == 0:
+			gram1 = line[0]
+			count = start_tag_count[gram1[1]] 
+			start_tag_count[gram1[1]] = count + 1
+		else:
 			gram1 = line[i-1]
-		token1 = gram1[1]
+			token1 = gram1[1]
 
-		gram2 = line[i]
-		token2 = gram2[1] 
+			gram2 = line[i]
+			token2 = gram2[1] 
 
-		print "(%s, %s)" % (gram2[0], token_description_verbose[token2])
+			prob = tuple_probability[token1][token2]
+			tuple_probability[token1][token2] = prob+1
 
-		prob = tuple_probability[token1][token2]
-		tuple_probability[token1][token2] = prob+1
+			if is_number(gram2[0]):
+				prob = number_probability[token2]
+				number_probability[token2] = prob+1
+
+			count = tag_count[token2]
+			tag_count[token2] = count+1
 
 # Print out the probability results
 print "\nProbabilities: "
-print "   E   U   Q   F   M   T   D   B"
+print "   U   Q   F   M   T   D   B"
 for i in range(len(tuple_probability)):
 	row = tuple_probability[i]
 	print (token_description[i]),
 	for num in row:
-		print("%2d " % num),
+		print("%4d " % num),
 	print ""
 
-# Try to parse the untagged recipes
-untagged_files = os.listdir("recipes")
-tagged_tuple_lines = []
-for f in untagged_files:
-	text = open("recipes/" + f)
-	for line in text:
-		tagged_tuples = []
-		words = line.split()
-		prev_tag = Empty
-		for i in range(len(words)):
-			word = words[i]
-			array = tuple_probability[prev_tag]
-			best_tag = Boring
-			match = 0
-			for x in range(len(array)):
-				if array[x] > match:
-					match = array[x]
-					best_tag = x
-			prev_tag = best_tag
-			tagged_tuples.append((word, best_tag))
-		tagged_tuple_lines.append(tagged_tuples)
+def find_tag(word, prev_tag):
+	array = tuple_probability[prev_tag]
+	best_tag = Boring
+	match = 0
+	for x in range(len(array)):
+		if array[x] > match:
+			match = array[x]
+			best_tag = x
+	return best_tag
 
-for line in tagged_tuple_lines:
-	for pair in line:
-		print("(%s, %s)" % (pair[0], token_description_verbose[pair[1]])),
-	print "\n"
+def total_tag_count():
+	total = 0
+	for number in tag_count:
+		total+=number
+	return total
+
+def viderbi():
+
+	text = open("recipes/11.txt")
+	states = [0, 1, 2, 3, 4, 5, 6]
+
+	for line in text:
+		words = line.split()
+		V = []
+		path = []
+
+		for i in range(len(words)):
+			V.append([])
+			word = words[i]
+			
+			for state in states: #for each state
+				# There are 2 output probabilities:
+					# - A number
+					# - Not a number
+
+				output_prob = number_probability[state]/tag_count[state]
+				if is_number(word) == False:
+					output_prob = 1 - output_prob
+
+				if i == 0:
+					V[i].append(output_prob * start_tag_count[state]/sum(start_tag_count))
+					path.append([state])
+				else:
+					(prob, y0) = max((output_prob * tuple_probability[y0][state]/tag_count[y0] 
+						* V[i-1][y0], y0) for y0 in states)
+					V[i].append(prob)
+					path[state].append(y0)
+
+		if (len(words) == 0):
+			continue
+
+		(prob, state) = max([(V[len(words) - 1][y], y) for y in states])
+		final_path = path[state]
+
+		for i in range(len(words)):
+				print("("+words[i] + ": " + token_description_verbose[final_path[i]] + ")"),
+		print ""
+
+viderbi()
 
 
 
