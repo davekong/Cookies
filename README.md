508c0d0d843ec081360cda6434b4740e7fad966b
macNugs <niftynei@gmail.com>
2012-07-07 14:58:25 -0400

    3D probability model

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Chocolate Chip Cookies
 ======================
+Resources : 
+	- Another bloggers attempt to parse: http://blog.kitchenpc.com/2011/07/06/chef-watson/
+
+	- Online NLTK (with Python) : https://sites.google.com/site/naturallanguagetoolkit/book
--- cookies.py
+++ cookies.py
@@ -43,18 +43,21 @@ def parse_learning_data(f):
 				parsed_line.append((word_parts[0], Boring))
 		parsed_lines.append(parsed_line)
 
-# Initialize a table to hold probability relationships
+# Initialize a n-Dim table to hold probability relationships
+# Previous by Currrent by isNumber
 for row in range(8):
 	tuple_probability.append([])
 	for col in range(8):
-		tuple_probability[row].append(0)
+		tuple_probability[row].append([])
+		for num in range(2):
+			tuple_probability[row][col].append(0)
 
 # Load all the tagged_recipes file
 files = os.listdir("tagged_recipes")
 for f in files:
 	parse_learning_data(open("tagged_recipes/" + f))
 
-# Calculate the probability of a tuple occurance
+# Calculate the probability of a tuple occurrence
 for line in parsed_lines:
 	for i in range(len(line)):
 		gram1 = ("", Empty)
@@ -66,19 +69,24 @@ for line in parsed_lines:
 		token2 = gram2[1] 
 
 		print "(%s, %s)" % (gram2[0], token_description_verbose[token2])
-
-		prob = tuple_probability[token1][token2]
-		tuple_probability[token1][token2] = prob+1
+				
+		if gram2[0][0].isdigit():
+			tuple_probability[token1][token2][0] += 1
+		else:
+			tuple_probability[token1][token2][1] += 1
 
 # Print out the probability results
 print "\nProbabilities: "
-print "   E   U   Q   F   M   T   D   B"
 for i in range(len(tuple_probability)):
-	row = tuple_probability[i]
-	print (token_description[i]),
-	for num in row:
-		print("%2d " % num),
-	print ""
+	#print token_description[i] +"  E   U   Q   F   M   T   D   B"
+	print token_description[i] +"\t   N  ~N"
+	for j in range(len(tuple_probability[i])):
+		row = tuple_probability[i][j]
+		print ("\t" +token_description[j]),
+		for num in row:
+			print("%2d " % num),
+		print ""
+	print "---------------------------"
 
 # Try to parse the untagged recipes
 untagged_files = os.listdir("recipes")
@@ -95,17 +103,20 @@ for f in untagged_files:
 			best_tag = Boring
 			match = 0
 			for x in range(len(array)):
-				if array[x] > match:
-					match = array[x]
-					best_tag = x
+				if word[0].isdigit():
+					if array[x][0] > match:
+						match = array[x][0]
+						best_tag = x
+				else:
+					if array[x][1] > match:
+						match = array[x][1]
+						best_tag = x
 			prev_tag = best_tag
 			tagged_tuples.append((word, best_tag))
 		tagged_tuple_lines.append(tagged_tuples)
 
 for line in tagged_tuple_lines:
 	for pair in line:
-		print("(%s, %s)" % (pair[0], token_description_verbose[pair[1]])),
+			print("(%s, %s)" % (pair[0], token_description_verbose[pair[1]])),
 	print "\n"
-
-
 
